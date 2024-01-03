31f3c2d7a649ce4df4e2e3a975f96596e4aa97d7
gaofuhong <157297759@qq.com>
2024-01-03 15:46:54 +0800

    feat: add tic tac toe highlight

--- src/module/tic_tac_toe/components/Board.tsx
+++ src/module/tic_tac_toe/components/Board.tsx
@@ -1,43 +1,37 @@
 import Square from "./Square";
-import { createStyles } from "antd-style";
-
+import { Fragment } from 'react';
 interface Props {
   squares: string | null[];
+  highPath: number[];
   onClick: (i: number, coordinate: number[]) => void;
 }
 
-const useStyle = createStyles({
-  "board-row": {
-    "board-row:after": {
-      clear: "both",
-      content: '""',
-      display: "table",
-    },
-  },
-});
-const Board: React.FC<Props> = ({ squares, onClick }) => {
-  const { styles } = useStyle();
-
-  const renderSquare = (i: number, row: number, column: number) => {
-    return <Square value={squares[i]} onClick={() => onClick(i, [row, column])} />;
-  };
-
+const Board: React.FC<Props> = ({ squares, highPath, onClick }) => {
   const checkerboard = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
   ];
 
+  const renderSquare = (i: number, row: number, column: number) => {
+    return (
+      <Square
+        key={i}
+        value={squares[i]}
+        highLight={highPath.includes(i)}
+        onClick={() => onClick(i, [row, column])}
+      />
+    );
+  };
+
   return (
     <div>
       {checkerboard.map((item, index) => {
         return (
-          <>
-            {item.map((t, tIndex) =>
-              renderSquare(t, index+1, tIndex + 1),
-            )}
+          <Fragment key={index}>
+            {item.map((t, tIndex) => renderSquare(t, index + 1, tIndex + 1))}
             <br />
-          </>
+          </Fragment>
         );
       })}
     </div>
